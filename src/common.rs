014127252efa3129f5e35f82488743d81a2c57d1
fokx <36614441+fokx@users.noreply.github.com>
2025-05-09 20:08:05 +0800

    react to a multicast announce message by sending a message to a multicast group

--- src/common.rs
+++ src/common.rs
@@ -2,8 +2,10 @@
 use socket2::{Domain, Protocol, Socket, Type};
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::sync::Arc;
+use rcgen::{Certificate, KeyPair};
+use serde::Serialize;
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 pub struct Message {
     pub alias: String,
     pub version: String,
@@ -24,4 +26,36 @@ pub fn create_udp_socket(port: u16) -> std::io::Result<Arc<tokio::net::UdpSocket
     socket.join_multicast_v4(&addr, &Ipv4Addr::UNSPECIFIED)?;
     socket.bind(&SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port).into())?;
     Ok(Arc::new(tokio::net::UdpSocket::from_std(socket.into())?))
+}
+
+pub fn generate_fingerprint_cert(cert: Certificate) -> String {
+    use sha2::{Digest, Sha256};
+    let mut hasher = Sha256::new();
+    hasher.update(cert.pem());
+    let result = hasher.finalize();
+    let fingerprint = hex::encode(result);
+    fingerprint
+}
+pub fn generate_fingerprint_plain() -> String {
+    let length: u16 = 32;
+    use rand::Rng;
+    let mut rng = rand::rng();
+    let mut fingerprint = String::new();
+    for _ in 0..length {
+        let byte = rng.random_range(0..=255);
+        fingerprint.push_str(&format!("{:02x}", byte));
+    }
+    fingerprint
+}
+pub fn generate_cert_key() -> (Certificate, KeyPair) {
+    use rcgen::{generate_simple_self_signed, CertifiedKey};
+    // Generate a certificate that's valid for "localhost" and "hello.world.example"
+    let subject_alt_names = vec!["hello.world.example".to_string(),
+                                 "localhost".to_string()];
+
+    let CertifiedKey { cert, key_pair } = generate_simple_self_signed(subject_alt_names).unwrap();
+    // println!("{}", cert.pem());
+    println!("{}", key_pair.serialize_pem());
+ 
+    return (cert, key_pair);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,55 +2,85 @@ mod common;
 
 use common::{create_udp_socket, Message};
 use std::sync::Arc;
+use crate::common::generate_fingerprint_plain;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let port = 8848;
     let udp = create_udp_socket(port)?;
     let mut buf = [0; 1024];
+    let my_fingerprint = generate_fingerprint_plain();
+    let response = Message {
+        alias: "example".to_string(),
+        version: "1.0".to_string(),
+        device_model: Some("model_x".to_string()),
+        device_type: Some("type_y".to_string()),
+        fingerprint: my_fingerprint.clone(),
+        port,
+        protocol: "UDP".to_string(),
+        download: Some(true),
+        announce: false,
+    };
+    let addr: std::net::Ipv4Addr = "224.0.0.48".parse().unwrap();
 
     loop {
         let (count, remote_addr) = udp.recv_from(&mut buf).await?;
         let data = buf[..count].to_vec();
-
+        let udp_clone = Arc::clone(&udp);
+        let response_clone = response.clone(); // Cloning here is crucial
+        let my_fingerprint_clone = my_fingerprint.clone(); // Cloning here is crucial
+        
         tokio::spawn(async move {
             if let Ok(parsed) = serde_json::from_slice::<Message>(&data) {
+                if parsed.fingerprint == my_fingerprint_clone {
+                    println!("skip my own fingerprint");
+                    return;
+                }
                 println!("{:?}: {:?}", remote_addr, parsed);
+                udp_clone.send_to(
+                    &serde_json::to_vec(&response_clone).expect("Failed to serialize Message"),
+                    (addr, port),
+                ).await.expect("Send error");
             }
         });
     }
 }
 
+async fn announce() -> std::io::Result<()> {
+    let port = 8848;
+    let udp = create_udp_socket(port)?;
+    let addr: std::net::Ipv4Addr = "224.0.0.48".parse().unwrap();
+    let mut count = 0;
+    let fingerprint = generate_fingerprint_plain();
+    // loop {
+    println!("{}", count);
+    udp.send_to(
+        &serde_json::to_vec(&Message {
+            alias: format!("example {}", count).to_string(),
+            version: "1.0".to_string(),
+            device_model: Some("model_x".to_string()),
+            device_type: Some("type_y".to_string()),
+            fingerprint: fingerprint.to_string(),
+            port,
+            protocol: "UDP".to_string(),
+            download: Some(true),
+            announce: true,
+        })
+                .expect("Failed to serialize Message"),
+        (addr, port),
+    )
+            .await
+            .expect("cannot send message to socket");
+    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+    count += 1;
+    // }
+    Ok(())
+}
 
 #[tokio::test]
 async fn client_test() -> std::io::Result<()> {
     // cargo test -- --nocapture
     // https://stackoverflow.com/questions/25106554/why-doesnt-println-work-in-rust-unit-tests
-    let port = 8848;
-    let udp = create_udp_socket(port)?;
-    let addr: std::net::Ipv4Addr = "224.0.0.48".parse().unwrap();
-    let mut count = 0;
-
-    loop {
-        println!("{}", count);
-        udp.send_to(
-            &serde_json::to_vec(&Message {
-                alias: "example".to_string(),
-                version: "1.0".to_string(),
-                device_model: Some("model_x".to_string()),
-                device_type: Some("type_y".to_string()),
-                fingerprint: "unique_fingerprint".to_string(),
-                port,
-                protocol: "UDP".to_string(),
-                download: Some(false),
-                announce: true,
-            })
-                    .expect("Failed to serialize Message"),
-            (addr, port),
-        )
-                .await
-                .expect("cannot send message to socket");
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-        count += 1;
-    }
+    announce().await?;
+    Ok(())
 }
